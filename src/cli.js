26a90157211f1ce6e20c359dd74abfc24a3dcb30
Kanivets <nikita.kanivetz@yandex.ru>
2022-07-01 19:49:30 +0300

    fix brain-games

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,6 @@ import readlineSync from 'readline-sync';
 
 export const userName = () => {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name?');
-  console.log(`Hello,${name}!`);
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
